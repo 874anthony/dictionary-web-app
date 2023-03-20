31f988a5f5c32e916c13574b5cd9491fc684d1aa
Anthony Acosta M <anthony082008@hotmail.es>
2023-03-20 17:37:59 -0500

    Added input html design

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,10 @@ import './App.css'
 export default function App () {
   return <Layout>
     <Header />
+
+    <div className='search-box'>
+      <input className='input-search' type='text' placeholder='Search for any word...' />
+      <img className="input-search__icon" src='images/icon-search.svg' alt='Search Icon' />
+    </div>
   </Layout>
 }
